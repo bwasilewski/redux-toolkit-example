9b66b316d67203c34b9ffe42914ef92b5f93857b
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-01-02 14:43:40 -0600

    dont need to use a state variable here

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import store from '../../store'
 
-const Counter = () => {
-  const [count, setCount] = useState(store.getState().counter.value)
-
+const Counter = ({count}) => {
   const increment = () => {
     store.dispatch({ type: "counter/increment" })
-    setCount(store.getState().counter.value)
   }
 
   const decrement = () => {
     store.dispatch({ type: "counter/decrement" })
-    setCount(store.getState().counter.value)
+  }
+
+  const incrementByAmount = () => {
+    store.dispatch({ type: "counter/incrementByAmount", payload: 5 })
   }
 
   return (
@@ -21,9 +22,18 @@ const Counter = () => {
         <button onClick={decrement} type="button">Decrement</button>
         <h2>{count}</h2>
         <button onClick={increment} type="button">Increment</button>
+        <button onClick={incrementByAmount} type="button">
+          Increment by amount
+        </button>
       </div>
     </div>
   );
 }
  
-export default Counter
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter.value
+  }
+}
+
+export default connect(mapStateToProps)(Counter)
